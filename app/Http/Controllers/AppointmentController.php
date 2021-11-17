31662105b06d065a5ada886a15f566f44ee6cd39
Teun Buis <347956@student.fontys.nl>
2021-11-17 10:19:28 +0100

    Testing some back end stuff for the appointments

---
+++ app/Http/Controllers/AppointmentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AppointmentController extends Controller
+{
+
+        public function index(){
+            return view('appointment');
+        }
+        public function CreateAppointment(){
+
+
+        }
+        public function SaveAppointment(){
+
+        }
+}
